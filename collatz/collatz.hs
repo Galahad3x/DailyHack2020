d6f1abddb4ccd6dbf938a8c220826fefce95e63f
Galahad3x <joelaumedes@gmail.com>
2020-10-28 09:30:50 +0100

    reduit a 152

--- collatz/collatz.hs
+++ collatz/collatz.hs
@@ -1,4 +1,4 @@
 c 1 = 1
-c n = if even n then 1 + c (n `div` 2) else 1 + c (3*n + 1)
+c n = if odd n then 1 + c (3*n + 1) else 1 + c (div n 2)
 p n m = if c n == m then n else p (n+1) m
-main = print $ p 1 $ maximum $ map c $ [1,2..100000]
+main = print $ p 1 $ maximum $ map c [1..100000]
--- collatz/collatz.py
+++
@@ -1,5 +0,0 @@
-def c(n):
-	if n == 1:
-		return 1
-	elif n % 2 == 0:
-		return 
