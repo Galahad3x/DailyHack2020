c64ca104c5328a06ae828c0d2c3443719d2bd378
Galahad3x <joelaumedes@gmail.com>
2020-11-06 23:35:28 +0100

    afegit copies i per evaluar, falta poder expandir totes les possibilitats

--- bibliocat/biblio.py
+++ bibliocat/biblio.py
@@ -11,34 +11,47 @@ class Library:
 		self.books = []
 		self.distances = distances
 
+	def id_copy(self):
+		my_lib = Library(self.id, self.distances)
+		my_lib.books = [bk.id for bk in self.books]
+		return my_lib
+
 	def send_book(self, book, library, by_book_id=False):
-		if by_book_id:
-			for i, elem in self.books:
-				if elem.id == book:
-					book = self.books.pop(i)
-					break
+		if book.library == self:
+			if by_book_id:
+				for i, elem in self.books:
+					if elem.id == book:
+						book = self.books.pop(i)
+						break
+			else:
+				for i, elem in self.books:
+					if elem.id == book.id:
+						self.books.pop(i)
+						break
+			book.time = self.distances[library.id]
+			book.status = TRANSPORTING
+			book.destination = library
+			return 0
 		else:
-			for i, elem in self.books:
-				if elem.id == book.id:
-					self.books.pop(i)
-					break
-		book.time = self.distances[library.id]
-		book.status = TRANSPORTING
-		book.destination = library
+			return -1
 
 	def lend_book(self, book, reader, by_book_id=False):
-		if by_book_id:
-			for i, elem in self.books:
-				if elem.id == book:
-					book = self.books.pop(i)
-					break
+		if book.library == self and reader.library_id == self.id:
+			if by_book_id:
+				for i, elem in self.books:
+					if elem.id == book:
+						book = self.books.pop(i)
+						break
+			else:
+				for i, elem in self.books:
+					if elem.id == book.id:
+						self.books.pop(i)
+						break
+			reader.start_book(book)
+			book.destination = self
+			return 0
 		else:
-			for i, elem in self.books:
-				if elem.id == book.id:
-					self.books.pop(i)
-					break
-		reader.start_book(book)
-		book.destination = self
+			return -1
 
 
 class Book:
@@ -49,6 +62,25 @@ class Book:
 		self.time = 0
 		self.destination = None
 		self.reader = None
+		self.library = None
+
+	def id_copy(self):
+		my_book = Book(self.id, self.value)
+		my_book.status = self.status
+		my_book.time = self.time
+		if self.destination is None:
+			my_book.destination = None
+		else:
+			my_book.destination = self.destination.id
+		if self.reader is None:
+			my_book.reader = None
+		else:
+			my_book.reader = self.reader.id
+		if self.library is None:
+			my_book.library = None
+		else:
+			my_book.library = self.library.id
+		return my_book
 
 	def end_day(self):
 		if self.time != 0:
@@ -57,14 +89,23 @@ class Book:
 			if self.status == READING:
 				self.status = NOT_USED
 				self.destination.books.append(self)
+				self.library = self.destination
 				self.destination = None
 				self.reader.end_book(self)
 				return self.value
 			else:
 				self.status = NOT_USED
 				self.destination.books.append(self)
+				self.library = self.destination
 				self.destination = None
 				return 0
+
+	def get_library(self, libraries):
+		for lib in libraries:
+			if self.id in [bk.id for bk in lib.books]:
+				self.library = lib
+				return lib
+		return None
 
 
 class Reader:
@@ -74,16 +115,39 @@ class Reader:
 		self.books = [None, None, None]
 		self.book_times = {}
 
+	def id_copy(self):
+		my_reader = Reader(self.id, self.library_id)
+		my_reader.book_times = self.book_times
+		my_reader.books = []
+		for book in self.books:
+			if book is None:
+				my_reader.books.append(None)
+			else:
+				my_reader.books.append(book.id)
+		return my_reader
+
 	def start_book(self, book):
-		for i, bk in enumerate(self.books):
-			if bk is None:
-				book.reader = self
-				book.time = self.book_times[book.id]
-				book.status = READING
-				self.books[i] = book
+		if self.can_start_book():
+			for i, bk in enumerate(self.books):
+				if bk is None:
+					if book.status == NOT_USED:
+						book.reader = self
+						book.time = self.book_times[book.id]
+						book.status = READING
+						self.books[i] = book
+						return 0
+		return -1
 
 	def end_book(self, book):
 		for i, bk in enumerate(self.books):
 			if bk is not None:
 				if bk.id == book.id:
 					self.books[i] = None
+					return 0
+		return -1
+
+	def can_start_book(self):
+		for bk in self.books:
+			if bk is None:
+				return True
+		return False
--- bibliocat/simulation.py
+++ bibliocat/simulation.py
@@ -35,10 +35,115 @@ def read_file(input_filename):
 				for i, lib in enumerate(libs):
 					if lib.id == int(line_split[2]):
 						libs[i].books.append(my_book)
+				my_book.get_library(libs)
 
 	return time, libs, bks, rders
 
 
+def generate_possible_moves(bk, libs):
+	moves = []
+	if bk.status == biblio.NOT_USED:
+		for lib in libs:
+			if lib.id != bk.library.id:
+				moves.append(str(bk.id) + " m " + str(lib.id))
+	return moves
+
+
+def generate_possible_reads(bk, rdrs):
+	moves = []
+	if bk.status == biblio.NOT_USED:
+		for rd in rdrs:
+			if rd.library_id == bk.library.id:
+				if rd.can_start_book():
+					moves.append(str(bk.id) + " r " + str(rd.id))
+	return moves
+
+
+def find_all_possibilities(tm, libs, bks, rdrs):
+	if tm == 0:
+		return []
+	possibilities = ["end"]  # Passar al dia següent
+	for bk in bks:
+		possibilities += generate_possible_moves(bk, libs)
+		possibilities += generate_possible_reads(bk, rdrs)
+	print(possibilities)
+
+
+# Per cada moviment, tornar a expandir
+
+
+def evaluate_moves_with_ends_2(moves, libs, bks, rdrs):
+	# If not possible, return -1 score
+	value = 0
+	for move in moves:
+		if move == "end":
+			for bk in bks:
+				value += bk.end_day()
+		elif move.split()[1] == "m":
+			for bk in bks:
+				if bk.id == int(move.split()[0]):
+					for lib in libs:
+						if lib.id == int(move.split()[2]):
+							result = bk.library.send_book(bk, lib)
+							if result == -1:
+								return -1
+		elif move.split()[1] == "r":
+			for bk in bks:
+				if bk.id == int(move.split()[0]):
+					for rd in rdrs:
+						if rd.id == int(move.split()[2]):
+							result = rd.start_book(bk)
+							if result == -1:
+								return -1
+	return value
+
+
+def coordinated_deepcopy(libs, bks, rdrs):
+	# Create copies that keep IDs
+	libs_ref_ids = []
+	bks_ref_ids = []
+	rdrs_ref_ids = []
+	for lib in libs:
+		libs_ref_ids.append(lib.id_copy())
+	for bk in bks:
+		bks_ref_ids.append(bk.id_copy())
+	for rd in rdrs:
+		rdrs_ref_ids.append(rd.id_copy())
+	# Link every id with the object
+	for lib in libs_ref_ids:
+		book_ids = lib.books
+		for i, book_id in enumerate(book_ids):
+			for book in bks_ref_ids:
+				if book.id == book_id:
+					lib.books[i] = book
+	for bk in bks_ref_ids:
+		bk.get_library(libs_ref_ids)
+		if bk.destination is not None:
+			for lib in libs_ref_ids:
+				if lib.id == bk.destination:
+					bk.destination = lib
+		if bk.reader is not None:
+			for rd in rdrs_ref_ids:
+				if rd.id == bk.reader:
+					bk.reader = rd
+	for rdr in rdrs_ref_ids:
+		for i, bk_id in enumerate(rdr.books):
+			for bk in bks_ref_ids:
+				if bk.id == bk_id:
+					rdr.books[i] = bk
+	return libs_ref_ids, bks_ref_ids, rdrs_ref_ids
+
+
+def evaluate_moves_with_ends(moves, libs, bks, rdrs):
+	libs2, bks2, rdrs2 = coordinated_deepcopy(libs, bks, rdrs)
+	return evaluate_moves_with_ends_2(moves, libs2, bks2, rdrs2)
+
+
 time, libraries, books, readers = read_file(filename)
 
-print(libraries)
+# possible_answers = find_all_possibilities(time, libraries, books, readers)
+
+test_moves = ["0 r 0", "1 r 0", "2 r 0", "3 r 0"]
+print(evaluate_moves_with_ends(test_moves, libraries, books, readers))
+test_moves = ["0 r 0", "3 r 0", "1 r 0", "2 r 0"]
+print(evaluate_moves_with_ends(test_moves, libraries, books, readers))
